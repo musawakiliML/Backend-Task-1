7edff3d040a846c25b656d33530b99e63f849235
musawakiliML <musawakiliml@hotmail.com>
2022-10-27 22:27:47 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+@app.get('/')
+async def about() -> dict:
+    return {'slackusername': "Musawakiliml",
+            'backend': True,
+            'age': 26,
+            'bio': "Hey, there i am an aspiring Machine Learning Engineer."}
