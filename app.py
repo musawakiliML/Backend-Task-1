06ad99890256a1542b3e2746b1229fb8f941c649
musawakiliML <musawakiliml@hotmail.com>
2022-10-27 22:30:22 +0100

    updated

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ async def about() -> dict:
     return {'slackusername': "Musawakiliml",
             'backend': True,
             'age': 26,
-            'bio': "Hey, there i am an aspiring Machine Learning Engineer."}
+            'bio': "Hey, there i am an aspiring Machine Learning Engineer. A Python Developer with a zeal to become a grey haired programmer."}
