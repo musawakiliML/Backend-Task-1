138256ba3582c42c9e173f816a46e607741e740a
musawakiliML <musawakiliml@hotmail.com>
2022-10-29 01:45:54 +0100

    added cors support

--- api.py
+++ api.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-
 app = FastAPI()
+
+origins = [
+    "https://hngi9task1.onrender.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/api/v1/about')
 async def about() -> dict:
