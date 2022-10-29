017b799de2a0b10647153dccc8aa35e7285e65a5
musawakiliML <musawakiliml@hotmail.com>
2022-10-29 20:23:05 +0100

    updated README.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 HNGi9 Backend Task 1
 
 A Get api endpoint to return information.
+
+Start Command: uvicorn api:app --host 0.0.0.0 --port 10000
--- api.py
+++ api.py
@@ -1,21 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+#from starlette.middleware.cors import CORSMiddleware
+#from pydantic import BaseModel
 
 app = FastAPI()
 
-origins = [
-    "https://hngi9task1.onrender.com",
-    "https://hngi9task1.onrender.com/api/v1/about",
-    "http://0.0.0.0:10000",
-]
+origins = ['*']
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=origins,
+    #allow_credentials=False,
+    #allow_methods=["*"],
+    allow_headers=['*'],
 )
 
 @app.get('/api/v1/about')
